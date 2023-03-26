277c9863a1b3c97e0f397962b4414bcbee4e13c1
Mohammad Rochman Liulin Nuha <rochmanliulin@gmail.com>
2023-03-26 19:59:56 +0700

    merevisi fungsi perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 1));
 const subtract = (a, b) => a - b;
 console.log(subtract(5, 2));
 
-const multyply = (a, b) => a * b;
-console.log(multyply(3, 3));
+const multyplyNumber = (a, b) => a * b;
+console.log(multyplyNumber(3, 3));
 
 const divide = (a, b) => a / b;
 console.log(divide(10, 2));
